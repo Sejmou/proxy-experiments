e1aa8f26fc9c5a421a5056a11996139ce46a7f6f
Sejmou <samo.kolter@gmail.com>
2024-06-26 16:35:43 +0200

    update dummy APIs to return JSON w/ IP from which request came + proxy-related HTTP headers

--- api-server/app/main.py
+++ api-server/app/main.py
@@ -9,4 +9,8 @@ app = FastAPI()
 @app.get("/what-is-my-ip")
 def return_client_ip(request: Request):
     client_host = request.client.host
-    return f"Greetings from {NAME}! Your IP: {client_host}"
+    return {
+        "received_from": client_host,
+        "X-Forwarded-For": request.headers.get("x-forwarded-for"),
+        "X-Real-IP": request.headers.get("x-real-ip"),
+    }
